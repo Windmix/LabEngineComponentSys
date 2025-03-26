9f056c1194b3a9c1e80428a25c50618c3555a6f9
Windmix <95549790+Windmix@users.noreply.github.com>
2025-03-26 09:38:09 +0100

    Revert "init"
    This reverts commit 8d13db04378de39e8f172d2d3584f11885eb665c.

--- bin/shd/debug.fs
+++
@@ -1,8 +0,0 @@
-#version 430
-uniform vec4 color;
-out vec4 diffuseColor;
-
-void main()
-{
-	diffuseColor = color;
-}
--- bin/shd/fs_particles_bufstorage.glsl
+++
@@ -1,8 +0,0 @@
-#version 430
-in vec4 Color;
-out vec4 out_Color;
-
-void main()
-{
-	out_Color = Color;
-}
